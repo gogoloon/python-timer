61b2d7daabfc5288d88e3759539835e15ea55edf
gogolon <gogolon@syachiku.com>
2021-06-23 08:33:17 +0900

    Lesson2

--- timer_main.py
+++ timer_main.py
@@ -1,6 +1,60 @@
+import tkinter as tk
+
+
 class TimerMain():
-    def __init__(self):
-        pass
+    def __init__(self, timer_frame):
+
+        self.__frame = timer_frame
+        self.__start_status = True
+
+        self.__text_min = tk.StringVar()
+        self.__text_sec = tk.StringVar()
+        self.__remain_min = tk.StringVar()
+        self.__remain_sec = tk.StringVar()
+
+        self.__text_min.set(str(60))
+        self.__text_sec.set(str(0))
+        self.__remain_min.set(str("-"))
+        self.__remain_sec.set(str("-"))
+
+        self.__text_start_stop_button = tk.StringVar()
+        self.__text_start_stop_button.set("Start")
+
+        self.__scheduled_finish_time = tk.StringVar()
+        self.__scheduled_finish_time.set(str("-"))
 
     def click_timer_button(self):
-        print("Click!!!!")
+        self.__remain_sec.set(int(self.__text_sec.get()))
+        self.exec_timer()
+
+    def exec_timer(self):
+        __tmp_time_sec = int(self.__remain_sec.get())
+
+        __tmp_time_sec -= 1
+        self.__remain_sec.set(str(__tmp_time_sec))
+
+        self.__frame.after(1000, self.exec_timer)
+
+    @property
+    def text_min(self):
+        return self.__text_min
+
+    @property
+    def text_sec(self):
+        return self.__text_sec
+
+    @property
+    def text_start_stop_button(self):
+        return self.__text_start_stop_button
+
+    @property
+    def remain_min(self):
+        return self.__remain_min
+
+    @property
+    def remain_sec(self):
+        return self.__remain_sec
+
+    @property
+    def scheduled_finish_time(self):
+        return self.__scheduled_finish_time
--- timer_view.py
+++ timer_view.py
@@ -6,39 +6,40 @@ from timer_main import TimerMain
 
 class TimerView(ttk.LabelFrame):
     def __init__(self, master=None):
-        self.timer_main = TimerMain()
+
         __frame = ttk.LabelFrame(master, text="タイマー")
         __frame.pack(anchor="w", fill="x", expand=True, padx=30, pady=10)
+        self.timer_main = TimerMain(__frame)
 
         __label = Label(__frame, text="タイマー入力")
         __label.grid(row=0, column=0)
 
-        __label = Entry(__frame, justify="right", width="8", textvariable="")
+        __label = Entry(__frame, justify="right", width="8", textvariable=self.timer_main.text_min)
         __label.grid(row=0, column=1)
 
         __label = Label(__frame, text="分")
         __label.grid(row=0, column=2)
 
-        __label = Entry(__frame, justify="right", width="8", textvariable="")
+        __label = Entry(__frame, justify="right", width="8", textvariable=self.timer_main.text_sec)
         __label.grid(row=0, column=3)
 
         __label = Label(__frame, text="秒")
         __label.grid(row=0, column=4)
 
-        __label = Button(__frame, text="Start",
+        __label = Button(__frame, textvariable=self.timer_main.text_start_stop_button,
                          command=self.timer_main.click_timer_button)
         __label.grid(row=0, column=5)
 
         __label = Label(__frame, text="残時間")
         __label.grid(row=1, column=0)
 
-        __label = Label(__frame, text="<残りの分>")
+        __label = Label(__frame, textvariable=self.timer_main.remain_min)
         __label.grid(row=1, column=1)
 
         __label = Label(__frame, text="分")
         __label.grid(row=1, column=2)
 
-        __label = Label(__frame, text="<残りの秒>")
+        __label = Label(__frame,  textvariable=self.timer_main.remain_sec)
         __label.grid(row=1, column=3)
 
         __label = Label(__frame, text="秒")
@@ -47,5 +48,5 @@ class TimerView(ttk.LabelFrame):
         __label = Label(__frame, text="推定終了時刻：")
         __label.grid(row=1, column=5)
 
-        __label = Label(__frame, text="<推定終了時刻が入る>")
+        __label = Label(__frame, textvariable=self.timer_main.scheduled_finish_time)
         __label.grid(row=1, column=6)
